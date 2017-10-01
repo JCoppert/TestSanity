aa8f8ddf179aebdfd2ce9b0c3df8299f24633c79
Jordan Coppert <jordancoppert@gmail.com>
2017-10-01 16:36:06 -0700

    Added MapForItinerary on new branch

---
+++ TestSanity/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  TestSanity
+//
+//  Created by Jordan Coppert on 10/1/17.
+//  Copyright © 2017 CSC310Team22. All rights reserved.
+//
+
+import Foundation
+
+class mapForItinerary : NSObject {
+    
+}
